d10156d775d7a0b7880cfb48e5a0313fb16eea91
Johndoe <56298669+togane@users.noreply.github.com>
2021-12-10 18:59:53 +0900

    implement piece factory

--- src/utils/__tests__/fen.spec.ts
+++ src/utils/__tests__/fen.spec.ts
@@ -1,10 +1,18 @@
-import { createPieceColorFromSymbol } from '../fen';
+import { createPieceColorFromSymbol, createPieceFromSymbol } from '../fen';
+import { Pawn } from '@/entities';
 
 describe('FEN utils', () => {
   describe('createPieceColorFromSymbol', () => {
     it('should returns a piece color', () => {
       expect(createPieceColorFromSymbol('P')).toEqual('White');
       expect(createPieceColorFromSymbol('p')).toEqual('Black');
+    });
+  });
+
+  describe('createPieceFromSymbol', () => {
+    it('should returns a piece color', () => {
+      expect(createPieceFromSymbol('P')).toEqual(new Pawn('White'));
+      expect(createPieceFromSymbol('p')).toEqual(new Pawn('Black'));
     });
   });
 });
--- src/utils/fen.ts
+++ src/utils/fen.ts
@@ -1,13 +1,42 @@
+import { Bishop, King, Knight, Pawn, Piece, Queen, Rook } from '@/entities';
 import {
-  PieceSymbolOfFEN,
   ALL_WHITE_PIECE_SYMBOLS_OF_FEN,
   Color,
+  PieceSymbolOfFEN,
 } from '@/types';
 
 export const createPieceColorFromSymbol = (
   pieceSymbol: PieceSymbolOfFEN
 ): Color => {
-  return ALL_WHITE_PIECE_SYMBOLS_OF_FEN.includes(pieceSymbol)
+  return new Set<string>(ALL_WHITE_PIECE_SYMBOLS_OF_FEN).has(pieceSymbol)
     ? 'White'
     : 'Black';
+};
+
+// TODO
+// 記号からPieceのサブクラスを返す関数を作る
+// それと記号から色を返す関数と組み合わせて利用する
+// みたいに処理を切り出したい
+export const createPieceFromSymbol = (pieceSymbol: PieceSymbolOfFEN): Piece => {
+  const color = createPieceColorFromSymbol(pieceSymbol);
+  switch (pieceSymbol) {
+    case 'B':
+    case 'b':
+      return new Bishop(color);
+    case 'K':
+    case 'k':
+      return new King(color);
+    case 'N':
+    case 'n':
+      return new Knight(color);
+    case 'P':
+    case 'p':
+      return new Pawn(color);
+    case 'Q':
+    case 'q':
+      return new Queen(color);
+    case 'R':
+    case 'r':
+      return new Rook(color);
+  }
 };
