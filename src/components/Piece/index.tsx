f1dc0b365020fc476f04962b3cf53fc2395f4b91
Johndoe <56298669+togane@users.noreply.github.com>
2021-12-11 11:24:41 +0900

    implement piece component

--- src/components/Piece/index.tsx
+++ src/components/Piece/index.tsx
@@ -1,40 +1,41 @@
 import React from 'react';
+import { ReactSVG } from 'react-svg';
+
 import { PieceSymbolOfFEN } from '@/types';
-import black_bishop from 'assets/images/pieces/black/bishop.svg';
-import black_king from 'assets/images/pieces/black/king.svg';
-import black_knight from 'assets/images/pieces/black/knight.svg';
-import black_pawn from 'assets/images/pieces/black/pawn.svg';
-import black_queen from 'assets/images/pieces/black/queen.svg';
-import black_rook from 'assets/images/pieces/black/rook.svg';
-import white_bishop from 'assets/images/pieces/white/bishop.svg';
-import white_king from 'assets/images/pieces/white/king.svg';
-import white_knight from 'assets/images/pieces/white/knight.svg';
-import white_pawn from 'assets/images/pieces/white/pawn.svg';
-import white_queen from 'assets/images/pieces/white/queen.svg';
-import white_rook from 'assets/images/pieces/white/rook.svg';
+import BlackBishop from '@/assets/images/pieces/black/bishop.svg';
+import BlackKing from '@/assets/images/pieces/black/king.svg';
+import BlackKnight from '@/assets/images/pieces/black/knight.svg';
+import BlackPawn from '@/assets/images/pieces/black/pawn.svg';
+import BlackQueen from '@/assets/images/pieces/black/queen.svg';
+import BlackRook from '@/assets/images/pieces/black/rook.svg';
+import WhiteBishop from '@/assets/images/pieces/white/bishop.svg';
+import WhiteKing from '@/assets/images/pieces/white/king.svg';
+import WhiteKnight from '@/assets/images/pieces/white/knight.svg';
+import WhitePawn from '@/assets/images/pieces/white/pawn.svg';
+import WhiteQueen from '@/assets/images/pieces/white/queen.svg';
+import WhiteRook from '@/assets/images/pieces/white/rook.svg';
 
 type Props = {
   symbol: PieceSymbolOfFEN;
 };
 
 const images: {
-  [k in PieceSymbolOfFEN]: React.FC<React.SVGProps<SVGSVGElement>>;
+  [k in PieceSymbolOfFEN]: string;
 } = {
-  B: white_bishop,
-  K: white_king,
-  N: white_knight,
-  P: white_pawn,
-  Q: white_queen,
-  R: white_rook,
-  b: black_bishop,
-  k: black_king,
-  n: black_knight,
-  p: black_pawn,
-  q: black_queen,
-  r: black_rook,
+  B: WhiteBishop,
+  K: WhiteKing,
+  N: WhiteKnight,
+  P: WhitePawn,
+  Q: WhiteQueen,
+  R: WhiteRook,
+  b: BlackBishop,
+  k: BlackKing,
+  n: BlackKnight,
+  p: BlackPawn,
+  q: BlackQueen,
+  r: BlackRook,
 };
 
 export const Piece: React.VFC<Props> = ({ symbol }: Props) => {
-  return <img src={images[symbol]} />;
-  //return images[symbol];
+  return <ReactSVG src={images[symbol]} />;
 };
--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -11,8 +11,10 @@ declare module '*.png' {
 }
 
 declare module '*.svg' {
-  const content: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default content;
+  import React = require('react');
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
 }
 
 declare module '*.jpg' {
