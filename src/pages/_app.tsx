a9585dc9b5ae5c69c528d422ff1b3caf60608830
Johndoe <56298669+togane@users.noreply.github.com>
2022-01-03 16:39:54 +0900

    provide store to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+
+import { store } from '@/stores/store';
 
 import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />;
+    </Provider>
+  );
 }
 
 export default MyApp;
