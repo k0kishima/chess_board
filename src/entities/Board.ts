7d8a67509abc82e033371fa1ccd767194210ae69
Johndoe <56298669+togane@users.noreply.github.com>
2022-01-08 07:50:12 +0900

    implement a method which move a piece on the board

--- src/entities/Board.ts
+++ src/entities/Board.ts
@@ -34,6 +34,43 @@ export class Board {
     column[position.rank] = piece;
   }
 
+  movePiece(from: Position, destination: Position) {
+    const pieceOnTheFrom = this.pieces[from.file][from.rank];
+    if (pieceOnTheFrom === null) {
+      throw new Error('a piece is not on the specified from position.');
+    }
+
+    const pieceOnTheDestination =
+      this.pieces[destination.file][destination.rank];
+    if (
+      pieceOnTheDestination &&
+      pieceOnTheFrom.color === pieceOnTheDestination.color
+    ) {
+      throw new Error('a same color piece is on the specified destination.');
+    }
+
+    const attackablePositionStrings = pieceOnTheFrom
+      .attackablePositionsFrom(from)
+      .map((p) => p.toString());
+    const movablePositionStrings = pieceOnTheFrom
+      .movablePositionsFrom(from)
+      .map((p) => p.toString());
+    if (
+      !attackablePositionStrings.includes(destination.toString()) &&
+      !movablePositionStrings.includes(destination.toString())
+    ) {
+      throw new Error(
+        `${pieceOnTheFrom.toSymbol()} cannot move or attack to ${destination.toString()} from ${from.toString()}.`
+      );
+    }
+
+    // FIXME:
+    // 攻撃可能なマスに敵がいなくても移動できてしまう可能性があるので運用しながらデバッグする
+    // 移動可能なマス即ち攻撃も可能なマスみたいな駒だといいけど、例えばポーンなんかはそうではないのでそういったケースだとバグ
+    this.pieces[from.file][from.rank] = null;
+    this.pieces[destination.file][destination.rank] = pieceOnTheFrom;
+  }
+
   toFEN() {
     const reversedAllRanks = [...ALL_RANKS].reverse();
     const allFiles = [...ALL_FILES];
--- src/entities/__tests__/Board.spec.ts
+++ src/entities/__tests__/Board.spec.ts
@@ -20,6 +20,53 @@ describe('Board', () => {
     });
   });
 
+  describe('#movePiece', () => {
+    it('should raise an error when a piece is not on the specified from position', () => {
+      expect(() => {
+        const board = new Board();
+        board.movePiece(new Position('e', 2), new Position('e', 4));
+      }).toThrowError(
+        new Error('a piece is not on the specified from position.')
+      );
+    });
+
+    it('should raise an error when a same color piece on the specified destination', () => {
+      const board = new Board();
+      board.put(new Position('e', 2), new Pawn('White'));
+      board.put(new Position('e', 3), new Pawn('White'));
+      expect(() => {
+        board.movePiece(new Position('e', 2), new Position('e', 3));
+      }).toThrowError(
+        new Error('a same color piece is on the specified destination.')
+      );
+    });
+
+    it('should raise an error when specified piece cannnot move to destination', () => {
+      const board = new Board();
+      board.put(new Position('e', 2), new Pawn('White'));
+      expect(() => {
+        board.movePiece(new Position('e', 2), new Position('e', 5));
+      }).toThrowError(new Error('P cannot move or attack to e5 from e2.'));
+    });
+
+    it('should be able to move to blank destination by legal move', () => {
+      const board = new Board();
+      board.put(new Position('e', 2), new Pawn('White'));
+      board.movePiece(new Position('e', 2), new Position('e', 3));
+      // prettier-ignore
+      expect(board.pieces).toEqual({
+        a: { 1: null, 2: null, 3: null, 4: null, 5: null, 6: null, 7: null, 8: null },
+        b: { 1: null, 2: null, 3: null, 4: null, 5: null, 6: null, 7: null, 8: null },
+        c: { 1: null, 2: null, 3: null, 4: null, 5: null, 6: null, 7: null, 8: null },
+        d: { 1: null, 2: null, 3: null, 4: null, 5: null, 6: null, 7: null, 8: null },
+        e: { 1: null, 2: null, 3: new Pawn('White'), 4: null, 5: null, 6: null, 7: null, 8: null },
+        f: { 1: null, 2: null, 3: null, 4: null, 5: null, 6: null, 7: null, 8: null },
+        g: { 1: null, 2: null, 3: null, 4: null, 5: null, 6: null, 7: null, 8: null },
+        h: { 1: null, 2: null, 3: null, 4: null, 5: null, 6: null, 7: null, 8: null },
+      });
+    });
+  });
+
   describe('#toFEN', () => {
     it('should return FEN which was converted from pieces property', () => {
       const board = new Board();
