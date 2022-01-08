6d0f5fa41ed545865a965d4c62fb039ea1f4a9cd
Johndoe <56298669+togane@users.noreply.github.com>
2022-01-08 12:07:13 +0900

    implement undo button and redo button

--- src/features/board/components/ResetButton.tsx
+++ src/features/board/components/ResetButton.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
-import { boardSlice } from '../stores/slice';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRedo } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useDispatch } from 'react-redux';
+
+import { boardSlice } from '../stores/slice';
 
 export const ResetButton: React.FC = () => {
   const dispatch = useDispatch();
@@ -23,7 +24,7 @@ export const ResetButton: React.FC = () => {
 const Styled = styled.div`
   float: right;
   background-color: #fff;
-  border-radius: 50%;
+  border-radius: 100%;
   box-shadow: 0px 0px 2px #888;
   display: inline-block;
   padding: 0.5em 0.6em;
