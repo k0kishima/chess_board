02b54afd499dd64eb36129c2225f72fc1ece1930
Johndoe <56298669+togane@users.noreply.github.com>
2022-02-04 05:35:12 +0900

    add a method which adds castling flag to FEN

--- src/utils/FENBuilder.ts
+++ src/utils/FENBuilder.ts
@@ -1,15 +1,25 @@
-import { Color, FEN, File, Rank, ALL_FILES, ALL_RANKS } from '@/types';
+import {
+  CastlingMovement,
+  Color,
+  FEN,
+  File,
+  Rank,
+  ALL_FILES,
+  ALL_RANKS,
+} from '@/types';
 import { Board, Position } from '@/entities';
 
 export class FENBuilder {
   _piecePart: string | null;
   _activeColor: Color | null;
   _enPassantablePosition: Position | null;
+  _CastlingableFlag: string | null;
 
   constructor() {
     this._piecePart = null;
     this._activeColor = null;
     this._enPassantablePosition = null;
+    this._CastlingableFlag = null;
   }
 
   FEN(): FEN {
@@ -23,7 +33,7 @@ export class FENBuilder {
     return [
       this._piecePart,
       this._activeColor == 'White' ? 'w' : 'b',
-      '-',
+      this._CastlingableFlag,
       this._enPassantablePosition
         ? this._enPassantablePosition.toString()
         : '-',
@@ -40,6 +50,24 @@ export class FENBuilder {
 
   addEnPassantablePosition(position: Position | null) {
     this._enPassantablePosition = position;
+  }
+
+  addCastlingableFlag(castlingablePieces: CastlingMovement) {
+    console.log('add:', castlingablePieces);
+    const flags = [];
+    if (castlingablePieces.b1) {
+      flags.push('Q');
+    }
+    if (castlingablePieces.b8) {
+      flags.push('q');
+    }
+    if (castlingablePieces.g1) {
+      flags.push('K');
+    }
+    if (castlingablePieces.g8) {
+      flags.push('k');
+    }
+    this._CastlingableFlag = flags ? flags.join('') : '-';
   }
 
   _parseBoard(board: Board) {
