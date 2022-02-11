2dda0af9b494a7e4ccd7c749080a4fccc751f45a
Johndoe <56298669+togane@users.noreply.github.com>
2022-02-11 10:39:07 +0900

    add a factory like method which returns movable positions getter for specified move direction

--- src/utils/PieceMovement.ts
+++ src/utils/PieceMovement.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+import { Position } from '@/entities';
 import { ALL_MOVE_DIRECTIONS, MoveDirection } from '@/types';
 
 export const reverseOf = (moveDirection: MoveDirection) => {
@@ -6,4 +8,25 @@ export const reverseOf = (moveDirection: MoveDirection) => {
       ALL_MOVE_DIRECTIONS.length / 2) %
     ALL_MOVE_DIRECTIONS.length;
   return ALL_MOVE_DIRECTIONS[index];
+};
+
+export const movablePositionsGetterFor = (moveDirection: MoveDirection) => {
+  switch (moveDirection) {
+    case '↑':
+      return Position.allFrontsFrom;
+    case '↗':
+      return Position.allUpperRightsFrom;
+    case '→':
+      return Position.allRightsFrom;
+    case '↘':
+      return Position.allLowerRightsFrom;
+    case '↓':
+      return Position.allRearsFrom;
+    case '↙':
+      return Position.allLowerLeftsFrom;
+    case '←':
+      return Position.allLeftsFrom;
+    case '↖':
+      return Position.allUpperLeftsFrom;
+  }
 };
